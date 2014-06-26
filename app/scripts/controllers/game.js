da9a22135ea7789253430d7f91c51b4b247b51d2
Julius Cebreros <me@juliuscebreros.com>
2014-06-26 17:17:28 +0800

    Scheduler should properly work now

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -9,7 +9,10 @@ angular.module('partyanimalsDraftApp')
     $scope.turnsLeft = GameState.getTurnsLeft();
     $scope.totalCash = GameState.getInitialCash();
     $scope.showItinerary = false;
+    $scope.hours = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22, 23, 24];
     $scope.scheduledActivities = [];
+    $scope.futureLocation = $scope.human.hq;
+    $scope.currentLocation = $scope.human.hq;
 
     $http.get('/api/districts').success(function(districts){
       $scope.districts = districts;
@@ -40,25 +43,82 @@ angular.module('partyanimalsDraftApp')
 
     $scope.addToItinerary = function(activity){
       $scope.showItinerary = true;
-      if(activity.wasScheduled){
+      if(activity.type === 'MOVE'){
+        //change current location
+        $scope.futureLocation = activity.location;
+      }
+      if(activity.wasScheduled){ //remove schedule
         activity.wasScheduled = false;
         //remove from array
-        $scope.scheduledActivities = $scope.scheduledActivities.filter(function(val,i){
-          return !(val.id === activity.id);
-        });
-      }else{
+        removeFromSchedule(activity);
+        if(activity.type === 'MOVE'){
+          //TODO: if movement was removed remove every act inside this location until end of list or encounter a new move type
+          $scope.selectedDistrict.activities.forEach(function(val){
+            if(val.type !== 'MOVE')
+              val.disabled = true;
+          });
+        }
+      }else{ //add schedule
         activity.wasScheduled = true;
         $scope.scheduledActivities.push(activity);
+        if(activity.type === 'MOVE'){
+          $scope.selectedDistrict.activities.forEach(function(val){
+            if(val.type !== 'MOVE')
+              val.disabled = false;
+          });
+        }
       }
     }
 
-    $scope.changeSelected = function(district){
+    $scope.changeSelectedDistrict = function(district){
       if($scope.selectedDistrict){
         $scope.selectedDistrict.selected = false;
       }
       $scope.selectedDistrict = district;
       $scope.selectedDistrict.selected = true;
       $scope.selectedDistrict.kapitan = findKapitan($scope.selectedDistrict.kapitanId);
+      $scope.selectedDistrict.activities = [];
+      //load activities for this item
+      var moveActivity = null;
+      var inFutureLocation = $scope.futureLocation.id == $scope.selectedDistrict.id;
+
+      // if(!inFutureLocation){
+        //TODO: calculate cost based on location
+        moveActivity = {
+          id: -1,
+          type: 'MOVE',
+          name: 'Move Here',
+          cost: {
+            gold: 100,
+            hours: 3
+          },
+          disabled: inFutureLocation,
+          location: angular.copy($scope.selectedDistrict)
+        };
+        if(isInSchedule(moveActivity)){
+            moveActivity.wasScheduled = true;
+        }
+        toggleDisable(moveActivity, shouldDisable);
+      // }
+      var activities = $scope.activities.map(function(val){
+        var newVal = angular.copy(val);
+        newVal.location = angular.copy($scope.selectedDistrict);
+        //check if this is already selected
+        if(isInSchedule(newVal)){
+          console.log('Was already selected');
+          newVal.wasScheduled = true;
+        }
+        //check if we are in the future location
+        toggleDisable(newVal, shouldDisable);
+
+        return newVal;
+      });
+
+      if(moveActivity){
+          activities.unshift(moveActivity);
+      }
+
+      $scope.selectedDistrict.activities = activities;
     }
 
     $scope.onKapSelected = function(kapitan){
@@ -93,6 +153,34 @@ angular.module('partyanimalsDraftApp')
       $scope.state = 'home';
     }
 
+    $scope.$watch('scheduledActivities', function(newVal, oldVal){
+      //get the last movement location
+      var futureLocationFound = false;
+      for(var i = $scope.scheduledActivities.length-1; i >= 0; i--){
+        var val = $scope.scheduledActivities[i];
+        if(val.type === 'MOVE'){
+          $scope.futureLocation = val.location;
+          futureLocationFound = true;
+          break;
+        }
+      }
+      if(!futureLocationFound){
+        $scope.futureLocation = $scope.currentLocation;
+      }
+      //get the total time
+      var totalTime = $scope.scheduledActivities.reduce(function(a, b){
+        return a + b.cost.hours;
+      }, 0);
+      $scope.timeLeft = $scope.hours.length - totalTime;
+    }, true);
+
+    $scope.$watch('timeLeft', function(){
+      if(!$scope.selectedDistrict) return;
+      $scope.selectedDistrict.activities.forEach(function(val){
+        toggleDisable(val, shouldDisable);
+      });
+    }, true)
+
     var findKapitan = function(id){
       var retVal = null;
       $scope.kapitans.forEach(function(val){
@@ -101,5 +189,79 @@ angular.module('partyanimalsDraftApp')
         }
       });
       return retVal;
+    }
+
+    var isInSchedule = function(activity){
+      var retVal = false;
+      if(activity.type === 'MOVE'){
+        if(activity.location.id === $scope.futureLocation.id){
+            return true;
+        }else{
+          return false;
+        }
+      }
+      $scope.scheduledActivities.forEach(function(val){
+        if(val.id === activity.id && val.location.id === activity.location.id){
+          retVal = true;
+        }
+      });
+      return retVal;
+    }
+
+    var toggleDisable = function(activity, willDisable){
+      if(willDisable(activity)){
+        activity.disabled = true;
+      }else{
+        activity.disabled = false;
+      }
+    }
+
+    var shouldDisable = function(activity){
+        if(isInSchedule(activity)){
+          return false;
+        }
+        if(activity.type === 'MOVE'){
+          if(activity.location.id == $scope.futureLocation.id){
+            return true;
+          }else{
+            return $scope.timeLeft-activity.cost.hours < 0;
+          }
+        }
+        if($scope.timeLeft - activity.cost.hours < 0){
+          return true;
+        }
+        if(activity.location.id != $scope.futureLocation.id){
+          return true;
+        }
+    }
+
+    var removeFromSchedule = function(activity){
+      var i;
+      var length = 0;
+      var val;
+      for(i = $scope.scheduledActivities.length-1; i >= 0; i--){
+        val = $scope.scheduledActivities[i];
+        if(val.id === activity.id && val.location.id === activity.location.id){
+          break;
+        }
+      }
+      if(val.type === 'MOVE'){
+        //remove everything forward
+        for(var ii = i; ii < $scope.scheduledActivities.length; ii++){
+          var val2 = $scope.scheduledActivities[ii];
+          if(val2.location.id === activity.location.id){
+            length+=1;
+          }else{
+            break;
+          }
+        }
+        $scope.scheduledActivities.splice(i,length);
+      }else{
+        $scope.scheduledActivities.splice(i,1);
+      }
+
+      // $scope.scheduledActivities = $scope.scheduledActivities.filter(function(val,i){
+      //   return !(val.id === activity.id && val.location.id === activity.location.id);
+      // });
     }
   });
--- app/scripts/directives/itineraryview.js
+++ app/scripts/directives/itineraryview.js
@@ -6,11 +6,13 @@ angular.module('partyanimalsDraftApp')
       templateUrl: 'partials/itinerary-view.html',
       restrict: 'E',
       replace: true,
+      transclude: true,
       scope: {
-        scheduledActivities: '='
+        scheduledActivities: '=',
+        hours: '='
       },
       link: function postLink(scope, element, attrs) {
-        scope.hours = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];        
+
       }
     };
   });
