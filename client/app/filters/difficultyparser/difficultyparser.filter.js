2f3be20f74e32b1bf1643f243a71110ce4db66e1
Julius Cebreros <me@juliuscebreros.com>
2014-08-05 20:57:18 +0800

    Format stat UI

--- client/app/filters/difficultyparser/difficultyparser.filter.js
+++ client/app/filters/difficultyparser/difficultyparser.filter.js
@@ -4,6 +4,19 @@ angular.module('partyanimalsDraftApp')
   .filter('difficultyparser', function () {
     return function (input) {
       if(input < 20){
+        return 'VERY EASY';
+      }else if(input < 30){
+        return 'EASY';
+      }else if(input < 45){
+        return 'NORMAL';
+      }else{
+        return 'HARD';
+      }
+    };
+  })
+  .filter('demandparser', function(){
+    return function (input) {
+      if(input < 20){
         return 'VERY HIGH';
       }else if(input < 30){
         return 'HIGH';
