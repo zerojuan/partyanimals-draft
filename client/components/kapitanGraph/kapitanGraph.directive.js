be7e67ab281a597eaf7be7d9b20ccc5b74061bf8
Julius Cebreros <me@juliuscebreros.com>
2014-08-11 16:10:01 +0800

    Show relationship text in kapgraph

--- client/components/kapitanGraph/kapitanGraph.directive.js
+++ client/components/kapitanGraph/kapitanGraph.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('partyanimalsDraftApp')
-  .directive('kapitanGraph', function () {
+  .directive('kapitanGraph', function ($filter) {
     return {
       templateUrl: 'components/kapitanGraph/kapitanGraph.html',
       restrict: 'E',
@@ -38,6 +38,8 @@ angular.module('partyanimalsDraftApp')
           .style('background', function(d){
             return color(d);
           });
+        var pathLabel;
+        var highlightedLinks = [];
 
         var selectKapitan = function(d){
           scope.selectedKapitan = d;
@@ -55,6 +57,16 @@ angular.module('partyanimalsDraftApp')
           var l = this.getTotalLength(),
               i = d3.interpolateString('0,' + l, l + ',' + l);
           return function(t) { return i(t); };
+        }
+
+        function arcPath(leftHand, d) {
+            var start = leftHand ? d.source : d.target,
+                end = leftHand ? d.target : d.source,
+                dx = end.x - start.x,
+                dy = end.y - start.y,
+                dr = Math.sqrt(dx * dx + dy * dy),
+                sweep = leftHand ? 0 : 1;
+            return 'M' + start.x + ',' + start.y + 'A' + dr + ',' + dr + ' 0 0,' + sweep + ' ' + end.x + ',' + end.y;
         }
 
         scope.$watch('kapitans', function(){
@@ -117,16 +129,24 @@ angular.module('partyanimalsDraftApp')
               .start();
               // patterns.append('polygon')
               //   .attr('points', '5,0 10,10 0,10');
+            var link = svg.append('svg:g').selectAll('g.link')
+                .data(links)
+              .enter().append('g')
+                .attr('class', 'link');
 
-            var link = svg.selectAll('.link')
-                .data(links);
-            link.enter().append('path')
-                .attr('class', 'link')
+            var linkPath = link.append('svg:path')
+                .attr('class', function(d) { return 'link ' + d.type; })
                 .style('stroke', function(d){ return color(d.value);})
                 .style('stroke-width', function(d) {
                   return strokeWidth(d.value);
                 })
                 .style('stroke-opacity', 0.1);
+
+            var textPath = link.append('svg:path')
+                .attr('id', function(d) { return d.source.index + '_' + d.target.index; })
+                .style('stroke-opacity', 0)
+                .attr('class', 'textpath');
+
                 // .call(transition);
 
             var link2 = svg.selectAll('.link-2')
@@ -173,22 +193,42 @@ angular.module('partyanimalsDraftApp')
               //    .style('stroke', 'black')
               //    .style('fill', 'transparent')
               //    .style('stroke-width', 5);
+            svg.append('svg:g').attr('class', 'labels');
+
             var showHighlight = function(d, context){
               selectKapitan(d);
               d3.select(context).select('circle')
                 .transition()
                 .duration(500)
                 .style('fill', '#eee');
+              highlightedLinks = [];
               link2.each(function(l){
                 if(d.id < 0){
                   if(l.target.id === d.id){
                     d3.select(this).style('stroke-opacity', '1').call(transition);
+                    highlightedLinks.push(l);
                   }
                 }
-                if(l.source.id === d.id){
+                if(l.target.id === d.id || (l.source.id === d.id && l.target.id < 0)){
                   d3.select(this).style('stroke-opacity', '1').call(transition);
+                  highlightedLinks.push(l);
                 }
               });
+              svg.select('.labels').selectAll('.pathLabel')
+                .data(highlightedLinks)
+              .enter().append('svg:text')
+                .attr('class', 'pathLabel shadow')
+                .style('fill', 'black')
+                .append('svg:textPath')
+                  .attr('startOffset', '50%')
+                  .style('opacity', 0)
+                  .attr('text-anchor', 'middle')
+                  .attr('xlink:href', function(d) { return '/game#' + d.source.index + '_' + d.target.index; })
+                  .style('font-family', 'Arial')
+                  .text(function(d) { return $filter('feelings')(d.value); })
+                  .transition()
+                  .duration(1200)
+                  .style('opacity', 1);
             };
 
             var hideHighlight = function(d, context){
@@ -197,16 +237,22 @@ angular.module('partyanimalsDraftApp')
                 .transition()
                 .duration(500)
                 .style('fill', '#ccc');
+              highlightedLinks = [];
               link2.each(function(l){
                 if(d.id < 0){
                   if(l.target.id === d.id){
                     d3.select(this).style('stroke-opacity', '0');
+                    //highlightedLinks.push(l);
                   }
                 }
-                if(l.source.id === d.id){
+                if(l.target.id === d.id || (l.source.id === d.id && l.target.id < 0)){
                   d3.select(this).style('stroke-opacity', '0');
+                  //highlightedLinks.push(l);
                 }
               });
+              svg.selectAll('.pathLabel')
+                .data(highlightedLinks)
+                .exit().remove();
             };
             node.on('mouseover', function(d){
               showHighlight(d, this);
@@ -228,28 +274,38 @@ angular.module('partyanimalsDraftApp')
               //     .attr('y1', function(d) { return d.source.y; })
               //     .attr('x2', function(d) { return d.target.x; })
               //     .attr('y2', function(d) { return d.target.y; });
-              link.attr('d', function(d) {
-                  var dx = d.target.x - d.source.x,
-                      dy = d.target.y - d.source.y,
-                      dr = Math.sqrt(dx * dx + dy * dy);
-                  return 'M' +
-                      d.source.x + ',' +
-                      d.source.y + 'A' +
-                      dr + ',' + dr + ' 0 0,1 ' +
-                      d.target.x + ',' +
-                      d.target.y;
+              linkPath.attr('d', function(d) {
+                return arcPath(true, d);
               });
+
+              textPath.attr('d', function(d) {
+                return arcPath(d.source.x < d.target.x, d);
+              });
+              // link.attr('d', function(d) {
+              //     var dx = d.target.x - d.source.x,
+              //         dy = d.target.y - d.source.y,
+              //         dr = Math.sqrt(dx * dx + dy * dy);
+              //     return 'M' +
+              //         d.source.x + ',' +
+              //         d.source.y + 'A' +
+              //         dr + ',' + dr + ' 0 0,1 ' +
+              //         d.target.x + ',' +
+              //         d.target.y;
+              // });
               link2.attr('d', function(d) {
-                  var dx = d.target.x - d.source.x,
-                      dy = d.target.y - d.source.y,
-                      dr = Math.sqrt(dx * dx + dy * dy);
-                  return 'M' +
-                      d.source.x + ',' +
-                      d.source.y + 'A' +
-                      dr + ',' + dr + ' 0 0,1 ' +
-                      d.target.x + ',' +
-                      d.target.y;
+                return arcPath(true, d);
               });
+              // link2.attr('d', function(arcPa) {
+              //     var dx = d.target.x - d.source.x,
+              //         dy = d.target.y - d.source.y,
+              //         dr = Math.sqrt(dx * dx + dy * dy);
+              //     return 'M' +
+              //         d.source.x + ',' +
+              //         d.source.y + 'A' +
+              //         dr + ',' + dr + ' 0 0,1 ' +
+              //         d.target.x + ',' +
+              //         d.target.y;
+              // });
               node.attr('transform', function(d){return 'translate('+d.x+','+d.y+')';});
             });
           }
