41cbf9f0fd4e41bc15b42bea41ea7e4a7ed6ab15
Julius Cebreros <me@juliuscebreros.com>
2014-08-15 15:39:22 +0800

    Selection dialog number

--- client/app/endgame/endgame.controller.js
+++ client/app/endgame/endgame.controller.js
@@ -6,20 +6,26 @@ angular.module('partyanimalsDraftApp')
     $scope.state = 'prep';
     $scope.humanVote = 0;
     $scope.aiVote = 0;
-    // $scope.totalReputations = [];
-    // for(var i = 0; i < 15; i++){
-    //   $scope.totalReputations.push({
-    //     turn: 15 - i,
-    //     ai: {
-    //       reputation: Math.random() * 15,
-    //       votes: Math.random() * 15
-    //     },
-    //     human: {
-    //       reputation: Math.random() * 15,
-    //       votes: Math.random() * 15
-    //     }
-    //   });
-    // }
+    //MOCK DATA
+    function mockData(){
+      $scope.totalReputations = [];
+      for(var i = 0; i < 15; i++){
+        $scope.totalReputations.push({
+          turn: 15 - i,
+          ai: {
+            reputation: Math.random() * 30,
+            votes: Math.random() * 30
+          },
+          human: {
+            reputation: Math.random() * 30,
+            votes: Math.random() * 30
+          }
+        });
+      }
+    }
+
+    //mockData();
+
 
     $scope.onPreparationDone = function(){
       $scope.state = 'sim';
