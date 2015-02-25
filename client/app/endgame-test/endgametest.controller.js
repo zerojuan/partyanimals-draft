eb9cce980f5192021c252772b9b5a24130f1df0d
Julius Cebreros <me@juliuscebreros.com>
2015-02-25 23:32:33 +1300

    Show district data

--- client/app/endgame-test/endgametest.controller.js
+++ client/app/endgame-test/endgametest.controller.js
@@ -2,5 +2,26 @@
 
 angular.module('partyanimalsDraftApp')
   .controller('EndgameTest', function ($scope) {
-    console.log('This is the scope now');
+    var District = function(name, population, playerRep, aiRep){
+      return {
+        name: name,
+        reputation: {
+          player: playerRep,
+          ai: aiRep
+        },
+        population: population
+      };
+    };
+
+    $scope.districts = [
+      new District('kapitolyo', 1000, 20, 23),
+      new District('casino', 1000, 60, 30),
+      new District('fishingvillage', 1000, 60, 30),
+      new District('port', 500, 50, 20),
+      new District('cathedral', 600, 20, 40),
+      new District('resort', 250, 20, 80)
+    ];
+
+
+
   });
